621a4a0e14e2f5b67af501757f3e0343d4b36642
Malash <i@malash.me>
2018-04-16 20:13:20 +0800

    init with rollup-module-boilerplate

---
+++ rollup.config.js
@@ -0,0 +1,65 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
+import { name, version, dependencies, devDependencies, peerDependencies } from './package.json';
+
+const target = process.env.TARGET || 'es';
+const env = process.env.NODE_ENV || 'development';
+const isProd = env === 'production';
+const banner = `/*
+ * @license
+ * ${name} v${version}
+ * (c) 2018-${new Date().getFullYear()} Malash <i@malash.me>
+ * Released under the MIT License.
+ */`;
+
+const config = {
+  input: 'src/index.js',
+  external: Object.keys(Object.assign({}, dependencies, devDependencies, peerDependencies)),
+  plugins: [
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      // .babelrc
+      presets: [
+        "react",
+        ['es2015', { modules: false }],
+        'stage-0',
+      ],
+      plugins: ['external-helpers'],
+    }),
+    commonjs(),
+    filesize(),
+  ],
+  output: {
+    banner,
+    file: `dist/${name}.${target}.${isProd ? 'min.js' : 'js'}`,
+    name: 'RpHoc',
+    format: target,
+  },
+};
+
+if (target === 'umd') {
+  config.external = Object.keys(Object.assign({}, peerDependencies));
+  config.plugins = [].concat(
+    [resolve()],
+    config.plugins,
+    [replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) })],
+  );
+}
+
+if (isProd) {
+  config.plugins = [].concat(
+    config.plugins,
+    [uglify({
+      output: {
+        comments: (node, comment) => comment.type === 'comment2' && /@preserve|@license|@cc_on/i.test(comment.value),
+      },
+    })],
+  );
+}
+
+export default config;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* Disable ESLint just for demo */
+/* eslint-disable */
+
+import React from 'react';
+
+export const App = ({ title }) => (
+  <h1>{title}</h1>
+);
